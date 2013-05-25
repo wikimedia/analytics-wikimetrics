188886b12bd947b93a034dcb2717b0ed66326b35
milimetric <dandreescu@wikimedia.org>
2013-05-25 07:12:39 -0400

    added db models

--- user_metrics/server/database/tables.py
+++ user_metrics/server/database/tables.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
@@ -20,42 +20,46 @@ class User:
         self.is_admin = is_admin
 
     def __repr__(self):
-        return '<Researcher("{0}","{1}", "{2}")>'.format(self.username, self.email, self.is_admin)
-    
-    
-#CREATE  TABLE IF NOT EXISTS `umapi`.`user` (
-  #`id` INT NOT NULL ,
-  #`username` VARCHAR(45) NULL ,
-  #`email` VARCHAR(255) NULL ,
-  #`role` VARCHAR(45) NULL ,
-  #PRIMARY KEY (`id`) ,
-  #UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
-  #UNIQUE INDEX `username_UNIQUE` (`username` ASC) )
-#ENGINE = InnoDB;
+        return '<User("{0}","{1}", "{2}")>'.format(self.username, self.email, self.is_admin)
 
 
-#CREATE  TABLE IF NOT EXISTS `umapi`.`user` (
-  #`id` INT NOT NULL ,
-  #`username` VARCHAR(45) NULL ,
-  #`email` VARCHAR(255) NULL ,
-  #`role` VARCHAR(45) NULL ,
-  #PRIMARY KEY (`id`) ,
-  #UNIQUE INDEX `email_UNIQUE` (`email` ASC) ,
-  #UNIQUE INDEX `username_UNIQUE` (`username` ASC) )
-#ENGINE = InnoDB;
+class Job:
+    __tablename__ = 'job'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    parent_job_id = Column(Integer, ForeignKey('job.id'))
+    classpath = Column(String(200))
+
+    def __init__(self, user_id, parent_job_id, classpath):
+        self.user_id = user_id
+        self.parent_job_id = parent_job_id
+        self.classpath = classpath
+
+    def __repr__(self):
+        return '<Job("{0}","{1}", "{2}")>'.format(self.user_id, self.parent_job_id, self.classpath)
 
 
-#CREATE  TABLE IF NOT EXISTS `umapi`.`job` (
-  #`id` INT NOT NULL ,
-  #`user_id` INT NOT NULL ,
-  #`arguments` TEXT NULL ,
-  #`parent_job_id` INT NULL ,
-  #`classpath` VARCHAR(200) NOT NULL ,
-  #PRIMARY KEY (`id`) ,
-  #INDEX `fk_research_result_researcher_idx` (`user_id` ASC) ,
-  #CONSTRAINT `fk_research_result_researcher`
-    #FOREIGN KEY (`user_id` )
-    #REFERENCES `umapi`.`user` (`id` )
-    #ON DELETE NO ACTION
-    #ON UPDATE NO ACTION)
-#ENGINE = InnoDB;
+class Cohort:
+    __tablename__ = 'cohort'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    description = Column(String(254))
+    default_project = Column(String(50))
+    created = Column(DateTime)
+    changed = Column(DateTime)
+    enabled = Column(Boolean)
+    public = Column(Boolean)
+
+    def __init__(self, username, email, is_admin):
+        self.name = name
+        self.description = description
+        self.default_project = default_project
+        self.created = created
+        self.changed = changed
+        self.enabled = enabled
+        self.public = public
+
+    def __repr__(self):
+        return '<Cohort("{0}","{1}","{2}","{3}","{4}","{5}","{6}")>'.format(self.name, self.description, self.default_project, self.created, self.changed, self.enabled, self.public)
