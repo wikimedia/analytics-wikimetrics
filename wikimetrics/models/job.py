5ecb803c99dacf9bd8a8ad9e524810968764f88c
Evan Rosen <erosen@wikimedia.org>
2013-06-12 08:44:41 -0700

    adds partial documention to models submodule

--- wikimetrics/models/job.py
+++ wikimetrics/models/job.py
@@ -12,12 +12,32 @@ __all__ = [
     'JobStatus',
 ]
 
+"""
+We use a tree-based job model to represent the relationships 
+between tasks which have a partial ordering, but which should
+still be loosely coupled or asynchronous.  For example, if we want
+to compute the average of some metric for a cohort, it makes sense
+to first compute the metric and then take the average.  In this example
+both the task of computing the metric and taking the average would be
+Jobs, and the averaging task would register the task of computing the
+actual metric as its child.
+
+Specifically, we distinguish between `JobLeaf` instances, which have no
+subjobs, and `JobNode` isntances, which require their children to be
+excecuted first, before carrying out their task.  Computing a simple
+metric would be a `JobLeaf`, whereas any aggregator would be a `JobNode`
+"""
+
+
 class JobStatus(object):
     CREATED = 'CREATED'
     STARTED = 'STARTED'
     FINISHED = 'FINISHED'
 
 class Job(Base):
+    """
+    Base class for all jobs
+    """
     __tablename__ = 'job'
     
     id = Column(Integer, primary_key=True)
