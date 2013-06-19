75204bcffc1a2cf0945604ca91b6e527bcd3335a
Evan Rosen <erosen@wikimedia.org>
2013-06-19 14:09:17 -0700

    adds import only mode which configures everything and runs nothing, using the default config file paths

--- wikimetrics/run.py
+++ wikimetrics/run.py
@@ -6,115 +6,29 @@ from .configurables import config_web, config_db, config_celery
 logger = logging.getLogger(__name__)
 
 
-##################################
-# global parser setup and options
-##################################
-parser = argparse.ArgumentParser('wikimetrics',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-)
-parser.add_argument('--override-config', '-o',
-    default = None,
-    help='override config file',
-    dest='override_config',
-)
-#parser.add_argument('func', default=lambda : None)
-subparsers = parser.add_subparsers(
-    dest='command',
-    title='subcommands',
-)
 
-
-##################################
-# TEST subparser setup and options
-##################################
-def test(args):
-    config_web(args)
-    config_db(args)
-    config_celery(args)
-    
-
-test_parser = subparsers.add_parser('test', help='runs nosetests')
-test_parser.set_defaults(func=test)
-test_parser.add_argument('--web-config', '-w',
-    default='config/web_config.py',
-    help='Flask config file',
-    dest='web_config',
-)
-test_parser.add_argument('--db-config', '-d',
-    default='config/db_config.py',
-    help='Database config file',
-    dest='db_config',
-)
-test_parser.add_argument('--celery-config', '-c',
-    default='wikimetrics/config/celery_config.py',
-    help='Celery config file',
-    dest='celery_config',
-)
-
-
-##################################
-# WEB subparser setup and options
-##################################
-def web(args):
+def config_web_mode(args):
     config_web(args)
     config_db(args)
     config_celery(args)
 
 
-web_parser = subparsers.add_parser('web', help='runs flask webserver')
-web_parser.set_defaults(func=web)
-web_parser.add_argument('--web-config', '-w',
-    default='config/web_config.py',
-    help='Flask config file',
-    dest='web_config',
-)
-web_parser.add_argument('--db-config', '-d',
-    default='config/db_config.py',
-    help='Database config file',
-    dest='db_config',
-)
-web_parser.add_argument('--celery-config', '-c',
-    default='wikimetrics/config/celery_config.py',
-    help='Celery config file',
-    dest='celery_config',
-)
+def config_test_mode(args):
+    config_web(args)
+    config_db(args)
+    config_celery(args)
 
 
-##################################
-# CELERY subparser setup and options
-##################################
-def celery(args):
+def config_celery_mode(args):
+    config_db(args)
+    config_celery(args)
+
+
+def config_import_only_mode(args):
+    config_web(args)
     config_db(args)
     config_celery(args)
     
-    from wikimetrics.models import ConcatMetricsJob
-    from wikimetrics.models import MultiProjectMetricJob
-    from wikimetrics.models import MetricJob
-
-
-celery_parser = subparsers.add_parser('celery', help='runs celery broker and workers')
-celery_parser.set_defaults(func=celery)
-celery_parser.add_argument('--celery-config', '-c',
-    default='config/celery_config.py',
-    help='Celery config file',
-    dest='celery_config',
-)
-celery_parser.add_argument('--db-config', '-d',
-    default='config/db_config.py',
-    help='Database config file',
-    dest='db_config',
-)
-
-
-##################################
-# This runs at import time to prepare
-# wikimetrics.configurables
-##################################
-args = parser.parse_args()
-logger.info('running with arguments:\n%s', pprint.pformat(vars(args)))
-
-# runs the appropriate config function (web, celery, test)
-args.func(args)
 
 
 ##################################
@@ -132,17 +46,88 @@ def run_test():
 
 def run_celery():
     from configurables import queue
+    from wikimetrics.models import ConcatMetricsJob
+    from wikimetrics.models import MultiProjectMetricJob
+    from wikimetrics.models import MetricJob
     queue.start()
+
+def setup_parser():
+    parser = argparse.ArgumentParser('wikimetrics',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument('--override-config', '-o',
+        default = None,
+        help='override config file',
+        dest='override_config',
+    )
+    parser.add_argument('mode',
+        nargs='?',
+        default='import', 
+        choices=[
+            'import',
+            'web',
+            'test',
+            'celery',
+        ],
+        help='web    : runs flask webserver'\
+            'test   : run nosetests'\
+            'celery : runs celery worker'
+            'import : configures everything and runs nothing',
+    )
+    parser.add_argument('--web-config', '-w',
+        default='config/web_config.py',
+        help='Flask config file',
+        dest='web_config',
+    )
+    parser.add_argument('--db-config', '-d',
+        default='config/db_config.py',
+        help='Database config file',
+        dest='db_config',
+    )
+    parser.add_argument('--celery-config', '-c',
+        default='wikimetrics/config/celery_config.py',
+        help='Celery config file',
+        dest='celery_config',
+    )
+    return parser
+
+
+
+
+##################################
+# This runs at import time to prepare
+# wikimetrics.configurables
+##################################
+parser = setup_parser()
+args = parser.parse_args()
+pprint.pprint(vars(args))
+logger.info('running with arguments:\n%s', pprint.pformat(vars(args)))
+
+# runs the appropriate config function (web, celery, test)
+if args.mode == 'web':
+    print 'running config_web_mode'
+    config_web_mode(args)
+elif args.mode == 'test':
+    config_test_mode(args)
+elif args.mode == 'celery':
+    config_celery_mode(args)
+elif args.mode == 'import':
+    print 'running config_import_only'
+    config_import_only_mode(args)
+
+
 
 
 def main():
     # runs the appropriate run function
-    if args.command == 'web':
+    if args.mode == 'web':
         run_web()
-    elif args.command == 'test':
+    elif args.mode == 'test':
         run_test()
-    elif args.command == 'celery':
+    elif args.mode == 'celery':
         run_celery()
+    elif args.mode == 'import':
+        pass
 
 
 if __name__ == '__main__':
