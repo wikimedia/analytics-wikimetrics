67b858594d2876968392b32ed177e7e76180f6ea
milimetric <dandreescu@wikimedia.org>
2013-07-09 12:35:17 -0400

    re-enabling queue in tests
    Change-Id: I20e8430c0a3d2d0b212296ba8e12c69a96a5a702

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,20 +20,20 @@ def celery_is_alive():
     return False
 
 
-#def setUp():
-    #celery_out = open(devnull, "w")
-    #celery_cmd = ['wikimetrics', '--mode', 'celery']
-    #global celery_proc
-    #celery_proc = Popen(celery_cmd, stdout=celery_out, stderr=celery_out)
+def setUp():
+    celery_out = open(devnull, "w")
+    celery_cmd = ['wikimetrics', '--mode', 'celery']
+    global celery_proc
+    celery_proc = Popen(celery_cmd, stdout=celery_out, stderr=celery_out)
     
-    ## wait until celery broker / worker is up
-    #tries = 0
-    #while(not celery_is_alive() and tries < 20):
-        #tries += 1
-        #sleep(0.5)
+    # wait until celery broker / worker is up
+    tries = 0
+    while(not celery_is_alive() and tries < 20):
+        tries += 1
+        sleep(0.5)
 
 
-#def tearDown():
-    #global celery_proc
-    #if celery_proc is not None:
-        #celery_proc.send_signal(SIGINT)
+def tearDown():
+    global celery_proc
+    if celery_proc is not None:
+        celery_proc.send_signal(SIGINT)
