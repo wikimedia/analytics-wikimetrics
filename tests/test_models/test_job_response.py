91a174db9fdb1ccba8e755f42de088e8ec67a91b
Evan Rosen <erosen@wikimedia.org>
2013-07-09 09:46:30 -0700

    adds test_job_response.py
    Change-Id: I95dcfc2ec7c9e19637d70bdb39df9d6ccadf96e4

---
+++ tests/test_models/test_job_response.py
@@ -0,0 +1,25 @@
+from wikimetrics.models import Cohort, JobResponse
+from wikimetrics.metrics import NamespaceEdits
+from ..fixtures import QueueDatabaseTest
+from nose.tools import assert_equals
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class JobResponseTest(QueueDatabaseTest):
+    
+    def test_empty_response(self):
+        # TODO: handle case where user tries to submit form with no cohorts / metrics
+        jr = JobResponse(cohort_metrics=[], user_id=0)
+        result = jr.run.delay().get()
+        assert_equals(result, [])
+    
+    
+    def test_basic_response(self):
+        c = self.session.query(Cohort).filter_by(name='test').one()
+        m = NamespaceEdits(namespaces=[0,1,2])
+        
+        jr = JobResponse(cohort_metrics=[(c,m)], user_id=0)
+        results = jr.run.delay().get()
+        print results
