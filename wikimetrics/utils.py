fbd7fb28ab0ae9fb20a5553dd66698a0b3ecc30b
milimetric <dandreescu@wikimedia.org>
2013-07-19 12:14:10 -0400

    decimal json fix
    Change-Id: Ib060ed9c970f18431272941e886b3dced577dc33

--- wikimetrics/utils.py
+++ wikimetrics/utils.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import decimal
 from time import mktime
 from flask import Response
 
@@ -8,9 +9,9 @@ def json_response(*args, **kwargs):
     """
     Handles returning generic arguments as json in a Flask application.
     Takes care of the following custom encoding duties:
-        * datetime.datetime objects encoded via DateTimeCapableEncoder
+        * datetime.datetime objects encoded via BetterEncoder
     """
-    data = json.dumps(dict(*args, **kwargs), cls=DateTimeCapableEncoder, indent=4)
+    data = json.dumps(dict(*args, **kwargs), cls=BetterEncoder, indent=4)
     return Response(data, mimetype='application/json')
 
 
@@ -30,19 +31,22 @@ def json_redirect(url):
     return json_response(isRedirect=True, redirectTo=url)
 
 
-class DateTimeCapableEncoder(json.JSONEncoder):
+class BetterEncoder(json.JSONEncoder):
     """
     Date/Time objects are not serializable by the built-in
     json library because there is no agreed upon standard of how to do so
     This class can be used as follows to allow your json.dumps to serialize dates properly.
     You should make sure your client is happy with this serialization:
-        print json.dumps(obj, cls=DateTimeCapableEncoder)
+        print json.dumps(obj, cls=BetterEncoder)
     """
     
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return int(mktime(obj.timetuple()))
-
+        
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
+        
         return json.JSONEncoder.default(self, obj)
 
 
