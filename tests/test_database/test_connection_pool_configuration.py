72ac421affa0c90183d9dde743cc79a91525fe12
Christian Aistleitner <christian@quelltextlich.at>
2014-07-25 13:52:20 +0200

    Put focus on last connection in connection pool test
    Change-Id: Iae4202594d27fdcd006978908bd2ad6e90c8712a

--- tests/test_database/test_connection_pool_configuration.py
+++ tests/test_database/test_connection_pool_configuration.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 from sqlalchemy.exc import TimeoutError
 from nose.plugins.attrib import attr
-from nose.tools import raises
 
 from tests.fixtures import mediawiki_project
 from wikimetrics.configurables import db, parse_db_connection_string, queue
@@ -12,7 +11,6 @@ from wikimetrics.models import Page
 class DatabaseSetupTest(TestCase):
 
     @attr('slow')
-    @raises(TimeoutError)
     def test_pool_size_can_be_exceeded(self):
         # The 11 in the following statement is 10 (default for
         # max_overflow) + 1. So this is the lowest value for which we
@@ -22,7 +20,18 @@ class DatabaseSetupTest(TestCase):
             db.get_mw_session(mediawiki_project)
             for i in range(pool_size)
         ]
-        [self.sessions[i].query(Page).first() for i in range(pool_size)]
+        [self.sessions[i].query(Page).first() for i in range(pool_size - 1)]
+
+        try:
+            self.sessions[-1].query(Page).first()
+
+            # We should never reach this, as the opening the
+            # connection for the above statement should fail.
+            self.fail("Final connection succeeded.")
+        except TimeoutError:
+            # The TimeoutError is expected, as it is above the limit
+            # of open connections in the pool.
+            pass
 
     def test_pool_size_is_used(self):
         pool_size = db.config['MEDIAWIKI_POOL_SIZE']
